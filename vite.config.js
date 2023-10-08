09474aeaf522f96eea75647abbbe1197b0a9bfb2
Eegor Badmayev <igorbadmaev2002@gmail.com>
2023-10-08 21:05:42 +0300

    del base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/CRUDily-the-CRUD-site/user",
+  // base: "/CRUDily-the-CRUD-site/",
   plugins: [react()],
 });
